1cdb403fb393c3d83dcb8a3e22e1c9b51d899e99
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-29 11:35:06 +0300

    Registered components

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
 import './App'
+import './components/About/About';
+import './components/Contacts/Contacts';
+import './components/Outlet/Outlet';
+import './components/Link/Link';
+import './components/NotFound/NotFound';
