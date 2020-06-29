b1797bf64eb927937f9f1f0b360e358a44a54075
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-29 11:32:35 +0300

    Clean

--- src/App.ts
+++ src/App.ts
@@ -1,24 +1,10 @@
 import { Component } from './packages/decorators/Component';
 import template from './App.html';
-import { State } from './packages/decorators/State';
 
 @Component({
-    selector: 'ex-home',
+    selector: 'exf-home',
     template
 })
-class Home {
-    @State
-    counter = 0;
-    @State
-    text = 1;
-    @State
-    inputValue = 1;
-
-    increase() {
-        this.counter++;
-    }
-
-    handleChange(e: any) {
-        this.text = e.target.value
-    }
+export class Home {
+    
 }
