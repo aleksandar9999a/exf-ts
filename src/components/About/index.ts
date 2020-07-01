935debe8ae9a10c132157a7a9570b7a5ba563826
aleksandar9999a <aleksandar9999a@gmail.com>
2020-07-01 12:32:29 +0300

    Added styles

--- src/components/About/index.ts
+++ src/components/About/index.ts
@@ -1,9 +1,26 @@
 import template from './template.html';
 import { Component } from '../../packages/decorators/Component';
+import { IStyleItem } from '../../packages/interfaces/interfaces';
+
+const styles: IStyleItem[] = [
+    {
+        selector: 'div',
+        styles: [
+            { 'background': 'green' }
+        ]
+    },
+    {
+        selector: '.foo',
+        styles: [
+            { 'color': 'red' }
+        ]
+    },
+]
 
 @Component({
     selector: 'exf-about',
-    template
+    template,
+    styles
 })
 export class About {
     
