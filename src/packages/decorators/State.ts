462e66637e91733bcc6a045cdad6e5257dd43451
aleksandar9999a <aleksandar9999a@gmail.com>
2020-08-30 13:07:06 +0300

    Change spaces to tabs

--- src/packages/decorators/State.ts
+++ src/packages/decorators/State.ts
@@ -1,28 +1,28 @@
 export function State(target: any, key: string, descriptor?: TypedPropertyDescriptor<any>) {
-    if (!!descriptor) {
-        const currentMethod = descriptor.value;
-        descriptor.value = function (this: any, ...args: any[]) {
-            currentMethod(...args);
-            
-            if (this.update) {
-                this.update();
-            }
-        }
-        return;
-    }
+	if (!!descriptor) {
+		const currentMethod = descriptor.value;
+		descriptor.value = function (this: any, ...args: any[]) {
+			currentMethod(...args);
+			
+			if (this.update) {
+				this.update();
+			}
+		}
+		return;
+	}
 
-    let val: any;
+	let val: any;
 
-    Object.defineProperty(target, key, {
-        set(newValue) {
-            val = newValue;
-            
-            if (this.update) {
-                this.update();
-            }
-        },
-        get() {
-            return val;
-        }
-    });
+	Object.defineProperty(target, key, {
+		set(newValue) {
+			val = newValue;
+			
+			if (this.update) {
+				this.update();
+			}
+		},
+		get() {
+			return val;
+		}
+	});
 }
