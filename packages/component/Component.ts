f0daf8647a112893217e45351d70642ac05d4a5d
aleksandar9999a <aleksandar9999a@gmail.com>
2020-09-13 22:24:25 +0300

    Change space to tabs

--- packages/component/Component.ts
+++ packages/component/Component.ts
@@ -3,88 +3,88 @@ import { representationParser, ExFStylize, extractStyleChanges, extractChanges }
 import { pushWork } from '../workLoop/work-loop';
 
 export class Component extends HTMLElement {
-    private _root!: ShadowRoot;
-    private _representation!: IElementRepresentation;
-    private _html!: HTMLElement;
-    private _ctorStyle: ICtorStyle = {
-        styles: [],
-        content: [],
-    };
-    private _props: Props = {};
-    private _state: State = {};
-    private _styles: Styles = {};
+	private _root!: ShadowRoot;
+	private _representation!: IElementRepresentation;
+	private _html!: HTMLElement;
+	private _ctorStyle: ICtorStyle = {
+		styles: [],
+		content: [],
+	};
+	private _props: Props = {};
+	private _state: State = {};
+	private _styles: Styles = {};
 
-    connectedCallback() {
-        this._root = this.attachShadow({ mode: 'closed' });
-        this._representation = (this as any).render();
-        this._html = representationParser(this._representation);
+	connectedCallback() {
+		this._root = this.attachShadow({ mode: 'closed' });
+		this._representation = (this as any).render();
+		this._html = representationParser(this._representation);
 
-        this._root.appendChild(this._html);
+		this._root.appendChild(this._html);
 
-        if (!!(this as any).stylize) {
-            const styleRep = (this as any).stylize();
-            this._ctorStyle = ExFStylize(styleRep.children);
+		if (!!(this as any).stylize) {
+			const styleRep = (this as any).stylize();
+			this._ctorStyle = ExFStylize(styleRep.children);
 
-            this._ctorStyle.styles.forEach((style) => {
-                this._root.appendChild(style);
-            });
-        }
+			this._ctorStyle.styles.forEach((style) => {
+				this._root.appendChild(style);
+			});
+		}
 
-        if (!!(this as any).onCreate) {
-            (this as any).onCreate();
-        }
-    }
+		if (!!(this as any).onCreate) {
+			(this as any).onCreate();
+		}
+	}
 
-    disconnectedCallback() {
-        if (!!(this as any).onDestroy) {
-            (this as any).onDestroy();
-        }
-    }
+	disconnectedCallback() {
+		if (!!(this as any).onDestroy) {
+			(this as any).onDestroy();
+		}
+	}
 
-    private updateStyle() {
-        if (!(this as any).stylize) {
-            return;
-        }
+	private updateStyle() {
+		if (!(this as any).stylize) {
+			return;
+		}
 
-        pushWork(() => {
-            const newRep = (this as any).stylize();
-            const { rep, commit } = extractStyleChanges(this._ctorStyle, newRep.children);
-            this._ctorStyle.content = rep;
+		pushWork(() => {
+			const newRep = (this as any).stylize();
+			const { rep, commit } = extractStyleChanges(this._ctorStyle, newRep.children);
+			this._ctorStyle.content = rep;
 
-            return commit;
-        });
-    }
+			return commit;
+		});
+	}
 
-    private update() {
-        if (!(this as any).render || !this._root) {
-            return;
-        }
+	private update() {
+		if (!(this as any).render || !this._root) {
+			return;
+		}
 
-        pushWork(() => {
-            const newRep = (this as any).render();
-            const changes = extractChanges((this._root as any) as ChildNode, [this._representation], [newRep]);
-            this._representation = newRep;
-            return changes;
-        });
-    }
+		pushWork(() => {
+			const newRep = (this as any).render();
+			const changes = extractChanges((this._root as any) as ChildNode, [this._representation], [newRep]);
+			this._representation = newRep;
+			return changes;
+		});
+	}
 
-    setAttribute(key: string, value: any, type?: string) {
-        this._props[key] = value;
+	setAttribute(key: string, value: any, type?: string) {
+		this._props[key] = value;
 
-        if (type === 'state') {
-            this.update();
-        } else if (type === 'style' && !!(this as any).stylize) {
-            this.updateStyle();
-        } else {
-            if (!!(this as any).stylize) {
-                this.updateStyle();
-            }
+		if (type === 'state') {
+			this.update();
+		} else if (type === 'style' && !!(this as any).stylize) {
+			this.updateStyle();
+		} else {
+			if (!!(this as any).stylize) {
+				this.updateStyle();
+			}
 
-            this.update();
-        }
-    }
+			this.update();
+		}
+	}
 
-    getAttribute(key: string) {
-        return this._props[key];
-    }
+	getAttribute(key: string) {
+		return this._props[key];
+	}
 }
